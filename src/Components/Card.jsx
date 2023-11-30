47c455924756fc7ac69a801770c9328a991ad5d2
DawidRaczek <dawid.raczek6666@gmail.com>
2023-11-30 19:08:47 +0100

    add spekaer button for future audioscription

--- src/Components/Card.jsx
+++ src/Components/Card.jsx
@@ -1,12 +1,30 @@
-import React, { useState } from "react";
+import React, {useState} from "react";
+import {VolumeUp} from "@mui/icons-material";
 
-export default function Card({ card }) {
+export default function Card({card}) {
     const [side, setSide] = useState(false);
 
+    function handleClickSpeaker(e) {
+        e.stopPropagation();
+        console.log('abc');
+    }
+
     return (
-        <div className={`card ${side ? "side" : ""}`} onClick={() => setSide(!side)}>
-            <div className="front">{card.pl}</div>
-            <div className="back">{card.en}</div>
+        <div className='cardWrapper'>
+            <div className={`card ${side ? "side" : ""}`} onClick={() => setSide(!side)}>
+                <div className="front">{card.pl}
+                    <VolumeUp
+                        className='speaker'
+                        onClick={handleClickSpeaker}
+                    ></VolumeUp>
+                </div>
+                <div className="back">{card.en}
+                    <VolumeUp
+                        className='speaker'
+                        onClick={handleClickSpeaker}
+                    ></VolumeUp>
+                </div>
+            </div>
         </div>
     );
 }
